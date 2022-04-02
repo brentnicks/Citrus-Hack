d54b473bfabb2c518c581461b660aea3d6aa7f4d
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 16:34:30 -0700

    4:34

---
+++ Grow-Your-Potential/Assets/Scripts/Planter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planter : MonoBehaviour
+{
+    bool canPlant = true;
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                plantSeed();
+            }
+        }
+    }
+
+
+
+    public void plantSeed()
+    {
+        switch (gm.currentSeed)
+        {
+            case 1:
+                if (gm.seed1 > 0)
+                --gm.seed1;
+                break;
+            case 2:
+                if (gm.seed2 > 0)
+                --gm.seed2;
+                break;
+            case 3:
+                if (gm.seed3 > 0)
+                --gm.seed3;
+                break;
+            default:
+                Debug.Log("invalid");
+                break;
+        }
+    }
+}
