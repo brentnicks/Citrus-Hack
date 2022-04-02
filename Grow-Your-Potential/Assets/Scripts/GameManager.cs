09d9fef64c2724ea8e11408422a193340069d488
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 15:04:02 -0700

    3:03

--- Grow-Your-Potential/Assets/Scripts/GameManager.cs
+++ Grow-Your-Potential/Assets/Scripts/GameManager.cs
@@ -7,7 +7,16 @@ public class GameManager : MonoBehaviour
 {
     public int dayNumber = 1;
     public GameObject dayCounter;
+    public int currentSeed = 1;
+    public GameObject border1, border2, border3;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            changeSeed();
+        }
+    }
     public void removeAllEnemies()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
@@ -21,5 +30,42 @@ public class GameManager : MonoBehaviour
     {
         ++dayNumber;
         dayCounter.GetComponent<Text>().text = "Day: " + dayCounter;
+    }
+
+    public void changeSeed()
+    {
+        ++currentSeed;
+        if (currentSeed > 3) currentSeed = 1;
+
+        if (currentSeed == 1)
+        {
+            setWhite(border1);
+            setGray(border2);
+            setGray(border3);
+        }
+        else if (currentSeed == 2)
+        {
+            setGray(border1);
+            setWhite(border2);
+            setGray(border3);
+        }
+        else if (currentSeed == 3)
+        {
+            setGray(border1);
+            setGray(border2);
+            setWhite(border3);
+        }
+    }
+
+    void setGray(GameObject border)
+    {
+        border.GetComponent<Image>().color = Color.gray;
+        border.GetComponent<Outline>().enabled = false;
+    }
+
+    void setWhite(GameObject border)
+    {
+        border.GetComponent<Image>().color = Color.white;
+        border.GetComponent<Outline>().enabled = true;
     }
 }
