7092b8fcfe3584fcf8309daa16337f45154e1cdf
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 10:48:45 -0700

    added player movement

---
+++ Grow-Your-Potential/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("vertical") * speed);
+    }
+}
