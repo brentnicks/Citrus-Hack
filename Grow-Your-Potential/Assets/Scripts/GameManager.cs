26c9ca0ddccabc6c62c91edb4f05be0d123b6111
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 16:05:24 -0700

    4:05

--- Grow-Your-Potential/Assets/Scripts/GameManager.cs
+++ Grow-Your-Potential/Assets/Scripts/GameManager.cs
@@ -8,8 +8,16 @@ public class GameManager : MonoBehaviour
     public int dayNumber = 1;
     public GameObject dayCounter;
     public int currentSeed = 1;
-    public GameObject border1, border2, border3;
+    public GameObject border1, border2, border3, coinCounter;
+    public int coins = 5;
+    public int seed1 = 0, seed2 = 0, seed3 = 0;
+    public Text textSeed1, textSeed2, textSeed3;
 
+    private void Start()
+    {
+        updateCoins();
+        updateSeedText();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
@@ -67,5 +75,17 @@ public class GameManager : MonoBehaviour
     {
         border.GetComponent<Image>().color = Color.white;
         border.GetComponent<Outline>().enabled = true;
+    }
+
+    public void updateCoins()
+    {
+        coinCounter.GetComponent<Text>().text = "Coins: " + coins;
+    }
+
+    public void updateSeedText()
+    {
+        textSeed1.text = seed1.ToString();
+        textSeed2.text = seed2.ToString();
+        textSeed3.text = seed3.ToString();
     }
 }
---
+++ Grow-Your-Potential/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    public int cost;
+    public string item;
+    GameManager gm;
+    double distance;
+    GameObject player;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if (Input.GetKeyDown(KeyCode.E) && distance < 1)
+        {
+            buyItem();
+        }
+
+    }
+
+    public void buyItem()
+    {
+        if (gm.coins >= cost)
+        {
+            gm.coins -= cost;
+            
+            switch (item)
+            {
+                case "low":
+                    ++gm.seed1;
+                    break;
+                case "medium":
+                    ++gm.seed2;
+                    break;
+                case "high":
+                    ++gm.seed3;
+                    break;
+                default:
+                    Debug.Log("Invalid");
+                    break;
+            }
+            Debug.Log("coins: " + gm.coins);
+            Debug.Log("seed1: " + gm.seed1);
+            gm.updateSeedText();
+            gm.updateCoins();
+        }
+    }
+
+
+}
