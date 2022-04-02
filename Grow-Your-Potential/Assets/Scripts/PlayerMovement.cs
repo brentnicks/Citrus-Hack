688aa4274103a34eb9daecce8fa16bbf94e8a021
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 11:28:38 -0700

    11:28

--- Grow-Your-Potential/Assets/Scripts/PlayerMovement.cs
+++ Grow-Your-Potential/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
     public float health = 100;
+    public GameObject healthBar;
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        healthBar.GetComponent<Text>().text = "Health: " + health;
     }
     private void Update()
     {
