f36ecce0a0adec486fe8537ba4a80209a72573b7
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 13:26:48 -0700

    1:26

---
+++ Grow-Your-Potential/Assets/Scripts/Sleep.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleep : MonoBehaviour
+{
+    public GameManager gm;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("sleep");
+        }
+    }
+}
