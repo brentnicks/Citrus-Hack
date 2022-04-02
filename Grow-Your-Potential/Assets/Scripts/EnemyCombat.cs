3261727186c3f0e32ac4e3b25b3e7ffc2b35aea6
myong003 <matthew.yong0801@gmail.com>
2022-04-02 16:13:05 -0700

    some enemy stuff

--- Grow-Your-Potential/Assets/Scripts/EnemyCombat.cs
+++ Grow-Your-Potential/Assets/Scripts/EnemyCombat.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class EnemyCombat : MonoBehaviour
 {
     public int health;
+    public float attackDelay;
+    protected bool canAttack = true;
+
+    protected float attackTimer;
+    protected GameObject player;
+    protected EnemyMovement movement;
+
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    public virtual void attack(){
+
+    }
     public void takeDamage(){
         health--;
         if (health == 0){
@@ -16,6 +30,22 @@ public class EnemyCombat : MonoBehaviour
         if (coll.gameObject.tag == "Player"){
             Debug.Log("Hit player");
             // coll.GetComponent<PlayerCombat>().takeDamage();
+        }
+    }
+
+    void Update(){
+        Vector3 playerPos = player.transform.position;
+        Vector3 currPos = gameObject.transform.position;
+        Vector3 posDiff = playerPos - currPos;
+        if (Mathf.Abs(posDiff.magnitude) < movement.targetDistance){
+            attack();
+        }
+
+        if (attackTimer > attackDelay){
+            canAttack = true;
+        }
+        else{
+            attackTimer += Time.deltaTime;
         }
     }
 }
