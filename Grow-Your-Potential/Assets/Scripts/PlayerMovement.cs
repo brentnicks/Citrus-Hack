06debd3f81105508266f4d5a0b276a0576ae2e18
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 11:03:25 -0700

    player

--- Grow-Your-Potential/Assets/Scripts/PlayerMovement.cs
+++ Grow-Your-Potential/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+    public float health = 100;
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("vertical") * speed);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
     }
 }
