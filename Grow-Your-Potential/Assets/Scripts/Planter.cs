c46467a3b0dc0e8b98c5bca4c1da1df48cc5060b
brentnicks <48264066+brentnicks@users.noreply.github.com>
2022-04-02 23:34:38 -0700

    11:34

--- Grow-Your-Potential/Assets/Scripts/Planter.cs
+++ Grow-Your-Potential/Assets/Scripts/Planter.cs
@@ -7,6 +7,9 @@ public class Planter : MonoBehaviour
     bool canPlant = true;
     GameManager gm;
     public GameObject seed1, seed2, seed3;
+    public GameObject enemy1, enemy2, enemy3;
+    int seed = 0;
+    GameObject currSeed;
 
     private void Start()
     {
@@ -33,6 +36,7 @@ public class Planter : MonoBehaviour
                 if (gm.seed1 > 0)
                 {
                     --gm.seed1;
+                    seed = 1;
                     SpawnPlant(seed1);
                     canPlant = false;
                 }
@@ -42,6 +46,7 @@ public class Planter : MonoBehaviour
                 if (gm.seed2 > 0)
                 {
                     --gm.seed2;
+                    seed = 2;
                     SpawnPlant(seed2);
                     canPlant = false;
                 }
@@ -51,6 +56,7 @@ public class Planter : MonoBehaviour
                 if (gm.seed3 > 0)
                 {
                     --gm.seed3;
+                    seed = 3;
                     SpawnPlant(seed3);
                     canPlant = false;
                 }
@@ -66,6 +72,15 @@ public class Planter : MonoBehaviour
 
     public void SpawnPlant(GameObject seed)
     {
-        Instantiate(seed, transform.position, Quaternion.identity);
+        currSeed = Instantiate(seed, transform.position, Quaternion.identity);
+    }
+
+    public void hatchEnemy()
+    {
+        Destroy(currSeed);
+        if (seed == 1) Instantiate(enemy1, transform.position, Quaternion.identity);
+        if (seed == 2) Instantiate(enemy2, transform.position, Quaternion.identity);
+        if (seed == 3) Instantiate(enemy3, transform.position, Quaternion.identity);
+
     }
 }
